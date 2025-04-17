727bd695700e874783ecd69fa8bee7b1cb89cdcc
KumaraKabilan <130783984+kabixit@users.noreply.github.com>
2025-04-18 00:57:13 +0530

    .

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useForm } from "react-hook-form"
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+
 import {
     Dialog,
     DialogContent,
@@ -7,9 +11,47 @@ import {
     DialogFooter,
     DialogHeader,
     DialogTitle,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
+
+
+import{
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+}from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+
+
+const formSchema = z.object({
+    name:z.string().min(1,{
+        message:"Server name is required"
+    }),
+    imageUrl: z.string().min(1, {
+        message: "Server image is required"
+    })
+}); 
+
 
 export const InitialModal = () =>{
+    const form= useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues:{
+            name:"",
+            imageUrl: "",
+        }
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async(values: z.infer<typeof formSchema>) =>{
+        console.log(values);
+    }
+
     return (
         <Dialog open>
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
