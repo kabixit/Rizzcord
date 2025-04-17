53032d49f5dadb329b27183aff761ba31974791f
KumaraKabilan <130783984+kabixit@users.noreply.github.com>
2025-04-17 20:37:48 +0530

    Initial commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <><UserButton /><ModeToggle /></>
+  );
+}
